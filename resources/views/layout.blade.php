8912984a682a1618568709c701edbed5af749510
dionisvl <dionisvl3@gmail.com>
2020-03-12 22:49:29 +0300

    Canonical

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="canonical" href="@php echo env('CANONICAL').$_SERVER['REQUEST_URI'] @endphp" />
+
     @if(!empty($post))
         <title>{{$post->getTitle()}}</title>
         <meta property="og:title" content="{{$post->getTitle()}}"/>
