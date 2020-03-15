0f1854e218965c08d0f56150fbc2dd439d5ab8cc
den <dden_vlad@mail.ru>
2020-03-15 18:19:13 +0300

    laravel 7 fix

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,19 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Input;
 use App\Post;
+use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $q = htmlspecialchars(Input::get ( 'q' ), ENT_QUOTES);
-        $post = Post::where('title','LIKE','%'.$q.'%')
-            ->orWhere('description','LIKE','%'.$q.'%')
-            ->orWhere('content','LIKE','%'.$q.'%')->get();
-        if(count($post) > 0)
-            return view('pages.search')->withDetails($post)->withQuery ( $q );
-        else return view ('pages.search')->withMessage('No Details found. Try to search again !');
+        $q = htmlspecialchars($request->q, ENT_QUOTES);
+
+        $post = Post::where('title', 'LIKE', '%' . $q . '%')
+            ->orWhere('description', 'LIKE', '%' . $q . '%')
+            ->orWhere('content', 'LIKE', '%' . $q . '%')->get();
+
+        if (count($post) > 0)
+            return view('pages.search')->withDetails($post)->withQuery($q);
+        else
+            return view('pages.search')->withMessage('No Details found. Try to search again !');
     }
 }
