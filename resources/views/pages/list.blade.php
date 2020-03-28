a5f69f8982babd17ea0c258ce14a3628524c1c10
den <dden_vlad@mail.ru>
2020-03-28 18:55:07 +0300

    правка шаблона страницы тегов

--- resources/views/pages/list.blade.php
+++ resources/views/pages/list.blade.php
@@ -7,28 +7,27 @@
         <div class="row">
             <div class="col-md-8">
                 <div class="row">
-                @foreach($posts as $post)
-                    <div class="col-md-6">
-                        <article class="post post-grid">
-                            <div class="post-thumb">
-                                <a class="post-thumb-overlay-img" href="{{route('post.show', $post->slug)}}"><img src="{{$post->getImage()}}" alt=""></a>
-
-                                <a href="{{route('post.show', $post->slug)}}" class="post-thumb-overlay text-center">
-                                    <div class="text-uppercase text-center">View Post</div>
-                                </a>
-                            </div>
-                            <div class="post-content">
-                                <header class="entry-header text-center text-uppercase">
-                                    @if($post->hasCategory())
-		                            <h6><a href="{{route('category.show', $post->category->slug)}}"> {{$post->getCategoryTitle()}}</a></h6>
-		                            @endif
+                    @foreach($posts as $post)
+                        <div class="col-md-6">
+                            <article class="post post-grid">
+                                <div class="post-content">
+                                    <header class="entry-header text-center text-uppercase">
+                                        <a class="post-thumb-overlay-img"
+                                           href="{{route('post.show', $post->slug)}}"><img src="{{$post->getImage()}}"
+                                                                                           alt=""></a>
+                                        @if($post->hasCategory())
+                                            <h6>
+                                                <a href="{{route('category.show', $post->category->slug)}}"> {{$post->getCategoryTitle()}}</a>
+                                            </h6>
+                                        @endif
 
 
-                                    <h1 class="entry-title"><a href="{{route('post.show', $post->slug)}}">{{$post->title}}</a></h1>
+                                        <h1 class="entry-title"><a
+                                                href="{{route('post.show', $post->slug)}}">{{$post->title}}</a></h1>
 
 
-                                </header>
-                                <div class="entry-content">
+                                    </header>
+                                    <div class="entry-content">
                                     {!! $post->description !!}
 
                                     <div class="social-share">
