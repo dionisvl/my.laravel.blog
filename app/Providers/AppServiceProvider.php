682a8e547a0c1614d2d89bd8ca23e0b9c91d1f19
dionisvl <dionisvl3@gmail.com>
2020-09-06 12:35:20 +0300

    fix shop categories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,24 +14,19 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        view()->composer('pages._sidebar', function ($view) {
+        view()->composer('pages._sidebar', static function ($view) {
             $view->with('popularPosts', Post::getPopularPosts());
             $view->with('featuredPosts', Post::where('is_featured', 1)->take(3)->get());
             $view->with('recentPosts', Post::orderBy('date', 'desc')->take(4)->get());
             $view->with('categories', Category::all());
         });
 
-        view()->composer('admin._sidebar', function ($view) {
+        view()->composer('admin._sidebar', static function ($view) {
             $view->with([
                 'newCommentsCount' => Comment::where('status', 0)->count()
             ]);
-        });
-
-        view()->composer('shop.layout', function ($view) {
-            $categories = Category::orderBy('title', 'asc')->get();
-            $view->with('categories', $categories);
         });
     }
 
@@ -40,7 +35,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
--- packages/shop/src/ShopServiceProvider.php
+++ packages/shop/src/ShopServiceProvider.php
@@ -2,13 +2,14 @@
 
 namespace Dionisvl\Shop;
 
+use App\Category;
 use Dionisvl\Shop\Models\Order;
 use Illuminate\Support\ServiceProvider;
 
 
 class ShopServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
         $this->mergeConfigFrom(
             $this->packageDir() . '/src/config/shop.php',
@@ -16,26 +17,31 @@ class ShopServiceProvider extends ServiceProvider
         );
     }
 
-    protected function packageDir()
+    protected function packageDir(): string
     {
-        return realpath(__DIR__ . '/../');
+        return dirname(__DIR__) . '/';
     }
 
-    public function boot()
+    public function boot(): void
     {
         $this->loadRoutesFrom($this->packageDir() . '/routes.php');
         $this->loadViewsFrom($this->packageDir() . '/src/resources/views', 'shop');
         $this->loadFactoriesFrom(__DIR__ . '/database/factories');
         $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
-        view()->composer('admin._sidebar', function ($view) {
+        view()->composer('admin._sidebar', static function ($view) {
             $view->with([
                 'newOrdersCount' => Order::where('status', 0)->count()
             ]);
         });
+
+        view()->composer('shop::shop.layout', static function ($view) {
+            $categories = Category::orderBy('title', 'asc')->get();
+            $view->with('categories', $categories);
+        });
     }
 
-    private function registerSettings()
+    private function registerSettings(): void
     {
 
     }
