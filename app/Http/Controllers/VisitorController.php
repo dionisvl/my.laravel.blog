0ce8826d22c64ec93ffc5e9b66ae0b5a6fe8ad11
Denis Dudin <dden_vlad@mail.ru>
2021-01-13 09:43:12 +0300

    add referer_last to visitor

--- app/Http/Controllers/VisitorController.php
+++ app/Http/Controllers/VisitorController.php
@@ -9,6 +9,16 @@ class VisitorController extends Controller
 {
     private static $agent;
 
+    private static function getAgent(): Agent
+    {
+        if (self::$agent === null)
+        {
+            self::$agent = new Agent();
+        }
+
+        return self::$agent;
+    }
+
     /**
      * Create new visitor if it is not robot
      * and it has unique params as browser, platform etc.
@@ -18,40 +28,36 @@ class VisitorController extends Controller
     {
         $agent = self::getAgent();
 
-        if ($agent->isRobot()) {
+        if ($agent->isRobot())
+        {
             return false;
         }
 
         $ip = request()->ip();
         $browser = $agent->browser();
         $platform = $agent->platform();
+        $referer = $_SERVER['HTTP_REFERER'] ?? null;
 
         $visitor = Visitor::where('ip', $ip)
             ->where('browser', $browser)
             ->where('platform', $platform)
             ->first();
 
-        if (empty($visitor)) {
+        if (empty($visitor))
+        {
             $visitor = Visitor::create([
                 'ip' => $ip,
                 'browser' => $browser,
                 'platform' => $platform,
-                'referer' => $_SERVER['HTTP_REFERER'] ?? null,
+                'referer' => $referer,
+                'referer_last' => $referer,
             ]);
         }
 
-        $visitor->referer = $_SERVER['HTTP_REFERER'] ?? null;
+        $visitor->referer_last = $referer;
         $visitor->save();
 
         return $visitor;
     }
 
-    private static function getAgent(): Agent
-    {
-        if (self::$agent === null) {
-            self::$agent = new Agent();
-        }
-
-        return self::$agent;
-    }
 }
