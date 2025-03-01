252a9b21da24f8a323b240f0377e027e48b8d38f
Denis <dionisvl3@gmail.com>
2025-03-01 16:20:17 +0300

    Traefik: fixed perms

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ TARGET_DIR="app-laravel/api-laravel"
 find "$TARGET_DIR" -type f -exec chmod 644 {} +
 
 # Оставляем 755 только для .sh, .py и .php скриптов
-find "$TARGET_DIR" -type f \( -name "*.sh" -o -name "*.py" -o -name "*.php" \) -exec chmod 755 {} +
+find "$TARGET_DIR" -type f \( -name "*.sh" -o -name "*.py" \) -exec chmod 755 {} +
 
 echo "Файловые права в '$TARGET_DIR' исправлены."
